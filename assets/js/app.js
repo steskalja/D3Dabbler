4e2317228836da945ed8e49cac819a130861c6c7
John <steskalj@outlook.com>
2021-05-26 12:44:59 -0400

    Initial Page and code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,67 @@
-// @TODO: YOUR CODE HERE!
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 1024 - margin.left - margin.right,
+    height = 450 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#scatter")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("../assets/data/data.csv").then( data => {
+
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 100])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+  svg.append("text")
+        .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.bottom) + ")")
+        .style("text-anchor", "middle")
+        .text("Age of Individual");
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 30])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+    svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("% of Smokers");
+
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d=> x(d.age) )
+    .attr("cy", d=> y(d.smokes) )
+    .attr("r", width * .01)
+    .style("fill", "#69b3a2")
+  // Add Text
+  svg.append('g')
+    .selectAll("text")
+    .data(data)
+    .enter()
+    .append("text")
+    .text(d => d.abbr)
+    .attr("x", d => x(d.age) )
+    .attr("y", d => y(d.smokes) )
+    .attr("dx", "-.5em")
+    .attr("dy", ".40em")
+    .attr("font-size", ".5em")
+
+});
